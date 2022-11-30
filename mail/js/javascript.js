63fbab8a81a2b0b3d25f39eba49df73c5d91ecf0
lollomrt <lorenzomartini.mrt@outlook.it>
2022-11-30 19:39:20 +0100

    Bonus esercizio Mail finito - inizio esercizio dadi

--- mail/js/javascript.js
+++ mail/js/javascript.js
@@ -1,23 +1,32 @@
 //1. Dare all'utente la possibilità di inserire la mail
-let mail = prompt("Inserisci la tua mail")
-console.log(mail)
-
+let mail 
 // 2. Creare la lista di mail tra le quali controllare
 let mailList = ["lorenzomartini.mrt@outlook.it", "lmdesign@mail.it", "lols@gmail.it", "boolean@gmail.it"]
 
-// 3. controllare se la mail inserita dall'utente è presente nella lista precedentemente creata
-let flag = false
-for (let i=0; i < mailList.length; i++) {
-    let mailAddress = mailList[i]
-    if (mailAddress == mail){
-        flag = true
+let button_invia = document.getElementById("invia") 
+button_invia.addEventListener("click", function(){
+    mail = document.getElementById("mail").value
+
+    // 3. controllare se la mail inserita dall'utente è presente nella lista precedentemente creata
+    let flag = false
+    for (let i=0; i < mailList.length; i++) {
+        let mailAddress = mailList[i]
+        if (mailAddress == mail){
+            flag = true
+        }
     }
-}
 
-if (flag) {
-    console.log("puoi entrare")
-}
+    let risultato
+    let puoi_entrare = "puoi entrare"
+    let non_puoi_entrare = "non puoi entrare"
 
-else {
-    console.log("non puoi entrare")
-}
+    if (flag) {
+        risultato = puoi_entrare
+    }
+
+    else {
+        risultato = non_puoi_entrare
+    }
+
+    document.getElementById("risultato").innerHTML = `<h2>${risultato}</h2>`
+})
